2a704614fd78e85bcb998890ff95aa3c5918fe40
Douglas Ferreira <douglasmfe@gmail.com>
2020-11-09 19:30:35 -0300

    Service layer, component registration

---
+++ src/main/java/com/douglasmfe/webservices/services/UserService.java
@@ -0,0 +1,31 @@
+package com.douglasmfe.webservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.douglasmfe.webservices.model.User;
+import com.douglasmfe.webservices.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+
+	public User findById(Long id){
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}	
+
+}
