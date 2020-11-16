7e9a76fbb5bff8e7afdd48001387fdf5c20648e4
Douglas Ferreira <douglasmfe@gmail.com>
2020-11-16 14:47:09 -0300

    Exception handling - delete

--- src/main/java/com/douglasmfe/webservices/services/UserService.java
+++ src/main/java/com/douglasmfe/webservices/services/UserService.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.douglasmfe.webservices.model.User;
 import com.douglasmfe.webservices.repositories.UserRepository;
+import com.douglasmfe.webservices.services.exceptions.DatabaseException;
 import com.douglasmfe.webservices.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,7 +35,16 @@ public class UserService {
 	}
 	
 	public void delete(Long id){
-		repo.deleteById(id);
+		
+		try {
+			repo.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
